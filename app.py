03d605c98cddf8e862091ee6dba7b049c4415257
hanarooo <211.hanaru@gmail.com>
2024-02-03 16:25:17 +0900

    조원들 취합 최종 제출본

--- app.py
+++ app.py
@@ -1,125 +1,158 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, json, session
+from flask import Flask, render_template, request,redirect,url_for,session,jsonify
 import os
+import requests,os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from sqlalchemy.orm import relationship
 from datetime import datetime
-from json.decoder import JSONDecodeError
+from sqlalchemy import asc
+from sqlalchemy import desc
+from flask_migrate import Migrate
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'session_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'database.db')
+        'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
 
+class Member(db.Model):
+    member_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pwd = db.Column(db.String, nullable=False)
+    memos = relationship('Memo', backref='member', lazy=True)
+    todo_lists = relationship('Todo', backref='member', lazy=True)
+    address_books = relationship('AddressBook', backref='member', lazy=True)
+    diaries = relationship('Diary', backref='member', lazy=True)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class Memo(db.Model):
+    memo_id = db.Column(db.Integer, primary_key=True)
+    main_text = db.Column(db.Text, nullable=False)
+    edit_time = db.Column(db.String, default=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S'), nullable=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class AddressBook(db.Model):
+    address_id = db.Column(db.Integer, primary_key=True)
+    member_name = db.Column(db.String, nullable=False)
+    role_mbti = db.Column(db.Text, nullable=True)
+    image_url = db.Column(db.String, nullable=False)
+    project = db.Column(db.String, nullable=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class Diary(db.Model):
+    diary_id = db.Column(db.Integer, primary_key=True)
+    main_text = db.Column(db.Text, nullable=False)
+    edit_time = db.Column(db.String, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
 # 다이어리 시작
 @app.route('/diary')
 def diary_list():
-    all_diaries = Diary.query.order_by(desc(Diary.diaryId)).all()
-
-    if all_diaries:
-        diary_per_page = 10
-        total_diary = len(all_diaries)
-    
-        if total_diary > diary_per_page:
-            page = request.args.get('page', 1, type=int)
-            start_index = (page - 1) * diary_per_page
-            end_index = start_index + diary_per_page
-
-            current_page = all_diaries[start_index:end_index]
-
-            # 페이지 번호 계산
-            total_pages = (total_diary + diary_per_page - 1) // diary_per_page
-
-            return render_template('diary.html', data=current_page, page_num=total_pages, page=page, all_diaries=all_diaries)
-        else:
-            # 게시글이 10개 이하인 경우 페이지 번호를 숨기고 전체 데이터를 반환
-            return render_template('diary.html', data=all_diaries, page_num=1, all_diaries=all_diaries)
-    else:
-        return "저장된 일기가 없습니다."
-
-
-
-@app.route('/diary/search')
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        all_diaries = Diary.query.filter_by(member_id=member_id_to_query).order_by(desc(Diary.diary_id)).all()
+        return render_template('diary.html', all_diaries=all_diaries)
+    else :
+        return redirect(url_for('sign_in'))
+        
+@app.route('/search')
 def search():
-    query = request.args.get('query', '')
-    results = []
-
-    if query:
-        results = Diary.query.filter(db.func.lower(Diary.title).contains(
-            query.lower())).order_by(desc(Diary.diaryId)).all()
-    return render_template('diary_search.html', data=results)
-
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        query = request.args.get('query', '')
+        results = []
+        if query:
+            results = Diary.query.filter(db.func.lower(Diary.title).contains(
+            query.lower())).order_by(desc(Diary.diary_id)).all()
+            return render_template('diary_search.html', data=results)
+    else :
+        return redirect(url_for('sign_in'))
 
 @app.route('/diary/regist', methods=['GET', 'POST'])
 def diary_regist():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        main_text = request.form.get('mainText')
-        edit_time = datetime.now().strftime('%Y-%m-%d')
-
-        diary = Diary(title=title, mainText=main_text, editTime=edit_time)
-        db.session.add(diary)
-        db.session.commit()
-
-        return redirect(url_for('diary_list'))
-
-    return render_template('diary_regist.html')
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        if request.method == 'POST':
+            title = request.form.get('title')
+            main_text = request.form.get('mainText')
+            edit_time = datetime.now().strftime('%Y-%m-%d')
+            memberid = member_id_to_query
+            diary = Diary(title=title, main_text=main_text, edit_time = edit_time,member_id = memberid)
+            db.session.add(diary)
+            db.session.commit()
+            return redirect(url_for('diary_list'))
+        else :
+            return render_template('diary_regist.html')
+    else :
+        return redirect(url_for('sign_in'))
 
 
 @app.route('/diary/<num>', methods=['GET', 'POST'])
 def diary_detail(num):
-    diary = Diary.query.get_or_404(num)
+    member_id_to_query = session.get('member_id')
+    print("post:"+ member_id_to_query)
+    if member_id_to_query : 
+        diary = Diary.query.filter_by(diary_id=num).first()
+        if diary :
+            if request.method == 'POST':
+                new_title = request.form['title']
+                new_main_text = request.form['mainText']
+                diary.title = new_title
+                diary.main_text = new_main_text
+                db.session.commit()
+                return redirect(url_for('diary_list'))
+            else :
+                return render_template('diary_detail.html', diary=diary)
+        else :
+            return render_template('diary_detail.html', diary=diary)
+    else :
+        return redirect(url_for('sign_in'))
 
-    if request.method == 'POST':
-        new_title = request.form['title']
-        new_main_text = request.form['mainText']
-
-        diary.title = new_title
-        diary.mainText = new_main_text
-        db.session.commit()
-
-        return redirect(url_for('diary_list'))
-
-    return render_template('diary_detail.html', diary=diary)
 
 
 @app.route('/diary/delete/<diaryId>', methods=['post'])
 def delete_diary(diaryId):
     diary = Diary.query.get_or_404(diaryId)
+    print(diary.diary_id)
     db.session.delete(diary)
     db.session.commit()
     return jsonify({'success': True, 'redirect': url_for('diary_list')})
 
 
-class Diary(db.Model):
-    diaryId = db.Column(db.Integer, primary_key=True,
-                        autoincrement=True)  # 시퀀스로 증가
-    title = db.Column(db.String, nullable=False)
-    editTime = db.Column(db.String, nullable=False)
-    mainText = db.Column(db.Text)
+# class Diary(db.Model):
+#     diaryId = db.Column(db.Integer, primary_key=True,
+#                         autoincrement=True)  # 시퀀스로 증가
+#     title = db.Column(db.String, nullable=False)
+#     editTime = db.Column(db.String, nullable=False)
+#     mainText = db.Column(db.Text)
 
-    def __repr__(self):
-        return f"Diary(diaryId={self.diaryId}, title='{self.title}', text='{self.mainText}', editTime='{self.editTime}')"
+#     def __repr__(self):
+#         return f"Diary(diaryId={self.diary_id}, title='{self.title}', text='{self.main_text}', editTime='{self.edit_time}')"
 
 
 # 회원가입 및 로그인
-class Member(db.Model):
-    memberId = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    pwd = db.Column(db.String, nullable=False)
+# class Member(db.Model):
+#     member_id = db.Column(db.String, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     pwd = db.Column(db.String, nullable=False)
 
 
 def __repr__(self):
-    return f"Member(memberId={self.memberId}, name='{self.name}', pwd='{self.pwd}')"
+    return f"Member(member_id={self.member_id}, name='{self.name}', pwd='{self.pwd}')"
 
 # 로그인
 @app.route('/signIn', methods=['GET', 'POST'])
@@ -130,14 +163,14 @@ def sign_in():
     if request.method == 'POST':
         checkId = request.form.get('memberId')
         checkPw = request.form.get('pwd')
-
+        
         member = Member.query.filter_by(
-            memberId=checkId, pwd=checkPw).first()
-        print(member.memberId)
+            member_id=checkId, pwd=checkPw).first()
+        print(member.member_id)
         if member:
             # 로그인 성공 시 세션에 사용자 정보 저장
-            session['memberId'] = checkId
-            return redirect(url_for('dashboard'))
+            session['member_id'] = checkId
+            return redirect(url_for('index'))
         else:
             return render_template('signin.html', msg='아이디 또는 비밀번호를 확인해 주세요')
 
@@ -146,12 +179,11 @@ def sign_in():
 ##############################################################################################
 # 대시보드 페이지 - 로그인이 필요한 페이지
 
-
 @app.route('/dashboard')
 def dashboard():
-    if 'memberId' in session:
+    if 'member_id' in session:
         # 세션에 사용자 정보가 있다면 index 화면을 렌더링
-        return render_template('index.html', memberId=session['memberId'])
+        return render_template('index.html', member_id=session['member_id'])
     else:
         # 세션에 사용자 정보가 없다면 로그인 화면으로 리다이렉트
         return redirect(url_for('sign_in'))
@@ -160,7 +192,7 @@ def dashboard():
 @app.route('/logout')
 def logout():
     # 세션에서 사용자 정보 삭제
-    session.pop('memberId', None)
+    session.pop('member_id', None)
     return redirect(url_for('sign_in'))
 ###############################################################################################
 
@@ -172,7 +204,7 @@ def sign_up():
         newPwd = request.form.get('newPwd')
         newName = request.form.get('newName')
 
-        new_member = Member(memberId=newId, name=newName, pwd=newPwd)
+        new_member = Member(member_id=newId, name=newName, pwd=newPwd)
         db.session.add(new_member)
         db.session.commit()
 
@@ -188,7 +220,7 @@ def check_duplicate():
         data = request.get_json()
         newId = data.get('newId')
 
-        oldMember = Member.query.filter_by(memberId=newId).first()
+        oldMember = Member.query.filter_by(member_id=newId).first()
 
         if oldMember:
             print('duplicate')
@@ -197,11 +229,196 @@ def check_duplicate():
             print('success')
             return jsonify({'result': 'success'})
     return render_template('signup.html')
+    
+
+@app.route("/memo/",methods=["GET"])
+def memo():
+    print("memo")
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query :
+        memo_data = Memo.query.filter_by(member_id=member_id_to_query).order_by(Memo.edit_time.desc()).all()
+        memo_per_page = 6
+        page = request.args.get('page', 1, type=int)
+        start_index = (page - 1) * memo_per_page
+        end_index = start_index + memo_per_page
+
+        current_page_memo = memo_data[start_index:end_index]
+
+        total_memos = len(memo_data)
+        total_pages = (total_memos + memo_per_page - 1) // memo_per_page
+        return render_template('memo.html', data=current_page_memo, page_num=total_pages)
+    else:
+        return redirect(url_for('sign_in'))
+
+    
+    
+
+@app.route("/memo/", methods=["POST"])
+def handle_memo():
+    if request.form.get("_method") == "POST":
+        return create_memo()
+    elif request.form.get("_method") == "PUT":
+        return update_memo()
+    else:
+        return "Invalid request"
+
+def create_memo():
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        text = request.form.get("content")
+        edit_time = request.form.get("current_time")
+        memo_data = Memo(main_text=text, edit_time=edit_time, member_id=member_id_to_query)
+        if memo_data :
+            db.session.add(memo_data)
+            db.session.commit()
+            return redirect(url_for('memo'))
+        else :
+            return "Invalid request"
+    else :
+        return redirect(url_for('sign_in'))
 
 
-with app.app_context():
-    db.create_all()
+def update_memo():
+    print("update")
+    text_receive = request.form.get("content")
+    edit_time_receive = request.form.get("current_time")
+    memo_id_receive = request.form.get("memo_id")
+    memo_data = Memo.query.filter_by(memo_id=memo_id_receive).first()
+    memo_data.main_text = text_receive
+    memo_data.edit_time = edit_time_receive
+    db.session.add(memo_data)
+
+    db.session.commit()  
+    return redirect(url_for('memo'))
+
+@app.route("/memo/delete/<id>")
+def delete_memo(id):
+    print("delete")
+    print(id)
+    memo = Memo.query.filter_by(memo_id=id).first()
+    if memo:
+        db.session.delete(memo)
+        db.session.commit()
+        return redirect(url_for('memo'))
+    else:
+        return 'Memo not found', 404
+
+    # 예시 Adress
+
+# 추가한 데이터를 화면에 표시
+@app.route("/AddressBook/")
+def member():
+        member_id_to_query = session.get('member_id')
+        if member_id_to_query : 
+            member_list = AddressBook.query.filter_by(member_id = member_id_to_query)
+            return render_template('address.html', data=member_list)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/AddressBook/Add/",methods=["GET"])
+def add_member():
+    # form 에서 보낸 데이터 받아오기
+        member_name_receive = request.args.get("member_name")
+        role_mbti_receive = request.args.get("role_mbti")
+        image_url_receive = request.args.get("image_url")
+        address_id_receive = request.args.get("address_id")
+        project_receive = request.args.get("project")
+        member_id_receive = request.args.get("member_id")
+
+
+    # 데이터를 DB에 저장
+        adress = AddressBook (member_name=member_name_receive, role_mbti=role_mbti_receive, image_url=image_url_receive, address_id=address_id_receive, project=project_receive, member_id=member_id_receive)
+        db.session.add(adress)
+        db.session.commit()
+        print(project_receive)
+        return render_template('address.html')
+
+# Define the route to render the index.html template
+@app.route('/')
+def index():
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        todos = Todo.query.filter_by(member_id=member_id_to_query).order_by(asc(Todo.date)).all()
+        return render_template('todo.html', todos=todos)
+    else :
+        return redirect(url_for('sign_in'))
+# Define the route to add a new todo
+@app.route('/api/todolist/', methods=['POST'])
+def add_todo():
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        try:
+            data = request.get_json()
+            task = data['task']
+            date_str = data['date']
+            date = datetime.strptime(date_str, '%Y-%m-%d') if date_str else None
+            member_id = member_id_to_query
+            new_todo = Todo(task=task, completed=False, date=date, member_id=member_id)
+            db.session.add(new_todo)
+            db.session.commit()
+            sorted_todos = Todo.query.order_by(asc(Todo.date)).all()
+
+            todos_list = [
+                {
+                    "id": todo.id,
+                    "task": todo.task,
+                    "completed": todo.completed,
+                    "date": todo.date.strftime('%Y-%m-%d') if todo.date else None,
+                }
+                for todo in sorted_todos
+            ]
+            response_data = {
+                "task":task,
+                "success": True,
+                "message": "할 일이 성공적으로 추가되었습니다",
+                "id": new_todo.id,
+                "todos": todos_list
+            }
+
+            if new_todo.date:
+                response_data["date"] = new_todo.date.strftime('%Y-%m-%d')
+
+            return jsonify(response_data)
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)})
+    else :
+        return redirect(url_for('sign_in'))
+
+# Define the route to update a todo using PATCH
+@app.route('/api/todolist/<int:todo_id>', methods=['PATCH'])
+def update_todo(todo_id):
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        try:
+            todo = Todo.query.get(todo_id)
+            if todo:
+                data = request.get_json()
+                todo.completed = data.get('completed', False)
+                db.session.commit()
+                return jsonify({"success": True, "message": "할 일이 성공적으로 업데이트되었습니다"})
+            else:
+                return jsonify({"success": False, "message": "존재하지 않는 할 일입니다"})
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)})
+    else : 
+        return redirect(url_for('sign_in'))
+
+# Define the route to delete a todo using DELETE
+@app.route('/api/todolist/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    member_id_to_query = session.get('member_id')
+    if member_id_to_query : 
+        try:
+            todo = Todo.query.get(todo_id)
+            if todo:
+                db.session.delete(todo)
+                db.session.commit()
+                return jsonify({"success": True, "message": "할 일이 성공적으로 삭제되었습니다"})
+            else:
+                return jsonify({"success": False, "message": "존재하지 않는 할 일입니다"})
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)})
+    else : 
+        return redirect(url_for('sign_in'))
+
+if __name__ == '__main__':
+    app.run()
